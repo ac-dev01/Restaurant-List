5697e296064f059baaa2f0c52dea6cee1a63a867
ACDNDSN <ayush.chaurasia@hotmail.com>
2022-03-09 23:22:00 +0530

    setting up RestaurantDetailViewController Outlet and Displaying Data in Static Table View

--- Port_1/Controllers/RestaurantDetailViewController.swift
+++ Port_1/Controllers/RestaurantDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantDetailViewController: UITableViewController {
 
@@ -22,7 +23,6 @@ class RestaurantDetailViewController: UITableViewController {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var cuisineLabel: UILabel!
     @IBOutlet var headerAddressLabel: UILabel!
-    
     
         //Cell Two
     @IBOutlet var tableDetailLabel: UILabel!
@@ -41,11 +41,110 @@ class RestaurantDetailViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dump(selectedRestaurant as Any)
+        
+        initialize()
 
     }
     
     
     //MARK: HelperMethods
 
+}
+
+
+private extension RestaurantDetailViewController {
+    
+    func initialize() {
+        setupLabels()
+        createMap()
+    }
+    
+    func setupLabels() {
+        
+        guard let restaurant = selectedRestaurant else {
+            return
+        }
+        
+        
+        title = restaurant.name
+        nameLabel.text = restaurant.name
+        cuisineLabel.text = restaurant.subtitle
+        headerAddressLabel.text = restaurant.address
+        tableDetailLabel.text = "Table for 7, tonight at 10:00 PM"
+        addressLabel.text = restaurant.address
+        
+    }
+    
+    func createMap() {
+        guard let annotation = selectedRestaurant,
+              let long = annotation.long,
+              let lat = annotation.lat else {
+                  return
+              }
+        
+        let location = CLLocationCoordinate2D(latitude: lat,
+                                              longitude: long)
+        
+        takeSnapshot(with: location)
+    }
+    
+    func takeSnapshot(with location: CLLocationCoordinate2D) {
+        let mapSnapshotOptions = MKMapSnapshotter.Options()
+        var loc = location
+        
+        let polyline = MKPolyline(coordinates: &loc, count: 1)
+        
+        let region = MKCoordinateRegion(polyline.boundingMapRect)
+        mapSnapshotOptions.region = region
+        mapSnapshotOptions.scale = UIScreen.main.scale
+        mapSnapshotOptions.size = CGSize(width: 340, height: 208)
+        mapSnapshotOptions.showsBuildings = true
+        mapSnapshotOptions.pointOfInterestFilter = .includingAll
+        
+        let snapShotter = MKMapSnapshotter(options: mapSnapshotOptions)
+        
+        snapShotter.start { snapshot, error in
+            guard let snapshot = snapshot else {
+                return
+            }
+            
+            UIGraphicsBeginImageContextWithOptions(mapSnapshotOptions.size,
+                                                   true,
+                                                   0)
+            
+            snapshot.image.draw(at: .zero)
+            
+            let identifier = "custompin"
+            let annotation = MKPointAnnotation()
+            
+            annotation.coordinate = location
+            
+            let pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            pinView.image = UIImage(named: "custom-annotation")!
+            
+            let pinImage = pinView.image
+            
+            var point = snapshot.point(for: location)
+            
+            let rect = self.locationMapImageView.bounds
+            
+            if rect.contains(point) {
+                let pinCenterOffset = pinView.centerOffset
+                point.x -= pinView.bounds.size.width / 2
+                point.y -= pinView.bounds.self.height / 2
+                point.x += pinCenterOffset.x
+                point.y += pinCenterOffset.y
+                
+                pinImage?.draw(at: point)
+            }
+            
+            if let image = UIGraphicsGetImageFromCurrentImageContext() {
+                UIGraphicsEndImageContext()
+                DispatchQueue.main.async {
+                    self.locationMapImageView.image = image
+                }
+            }
+        }
+    }
 }
